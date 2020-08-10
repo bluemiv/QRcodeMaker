777f6adde800e7c3c2804d3e5b8bece96a7f699a
TaehongKim <public.devhong@gmail.com>
2020-08-10 22:57:31 +0900

    Add QR Code Maker (#1) [R/V] N/A

--- test/qrcode_unittest.py
+++ test/qrcode_unittest.py
@@ -13,57 +13,57 @@ class QRCodeUnitTest(unittest.TestCase):
 
     def test_custom_color_build(self):
         builder = qrcode_maker.QRCodeBuilder()
-        qr = builder.set_data(self.url).set_image_path("../result/normal_qrcode.png").set_color("white",
+        qr = builder.set_data(self.url).set_image_path("../sample/normal_qrcode.png").set_color("white",
                                                                                                 "black").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/border_qrcode.png").set_border(30).build()
+        qr = builder.set_image_path("../sample/border_qrcode.png").set_border(30).build()
         qr.save_img()
 
         builder = qrcode_maker.QRCodeBuilder()
-        qr = builder.set_data(self.url).set_image_path("../result/box_size_qrcode.png").set_box_size(30).set_color(
+        qr = builder.set_data(self.url).set_image_path("../sample/box_size_qrcode.png").set_box_size(30).set_color(
             "white", "black").build()
         qr.save_img()
 
     def test_theme_build(self):
         builder = qrcode_maker.QRCodeBuilder()
-        qr = builder.set_data(self.url).set_image_path("../result/red_qrcode.png").set_theme("red").build()
+        qr = builder.set_data(self.url).set_image_path("../sample/red_qrcode.png").set_theme("red").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/blue_qrcode.png").set_theme("blue").build()
+        qr = builder.set_image_path("../sample/blue_qrcode.png").set_theme("blue").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/gray_qrcode.png").set_theme("gray").build()
+        qr = builder.set_image_path("../sample/gray_qrcode.png").set_theme("gray").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/grape_qrcode.png").set_theme("grape").build()
+        qr = builder.set_image_path("../sample/grape_qrcode.png").set_theme("grape").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/green_qrcode.png").set_theme("green").build()
+        qr = builder.set_image_path("../sample/green_qrcode.png").set_theme("green").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/orange_qrcode.png").set_theme("orange").build()
+        qr = builder.set_image_path("../sample/orange_qrcode.png").set_theme("orange").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/violet_qrcode.png").set_theme("violet").build()
+        qr = builder.set_image_path("../sample/violet_qrcode.png").set_theme("violet").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/cyan_qrcode.png").set_theme("cyan").build()
+        qr = builder.set_image_path("../sample/cyan_qrcode.png").set_theme("cyan").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/lime_qrcode.png").set_theme("lime").build()
+        qr = builder.set_image_path("../sample/lime_qrcode.png").set_theme("lime").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/pink_qrcode.png").set_theme("pink").build()
+        qr = builder.set_image_path("../sample/pink_qrcode.png").set_theme("pink").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/indigo_qrcode.png").set_theme("indigo").build()
+        qr = builder.set_image_path("../sample/indigo_qrcode.png").set_theme("indigo").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/teal_qrcode.png").set_theme("teal").build()
+        qr = builder.set_image_path("../sample/teal_qrcode.png").set_theme("teal").build()
         qr.save_img()
 
-        qr = builder.set_image_path("../result/yellow_qrcode.png").set_theme("yellow").build()
+        qr = builder.set_image_path("../sample/yellow_qrcode.png").set_theme("yellow").build()
         qr.save_img()
 
 
